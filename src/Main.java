28f1b354dbbc2638227aa86e21d61dccf917ceee
Sergey Bogomolov <freebios@yandex.ru>
2025-07-17 23:13:56 +0300

    Реализована функциональность определения корректности пути к файлу.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+        do{
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory) {
+                System.out.println("Это путь к папке, а не к файлу. Попробуйте еще раз");
+                continue;
+            }
+            if(!fileExists){
+                System.out.println("Файл не существует. Попробуйте еще раз.");
+            } else {
+                count++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер "+count);
+            }
+        }
+        while(true);
     }
 }
